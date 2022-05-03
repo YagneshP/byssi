2b0839d775f31c1bdb03e1ef45cf45ce38c8b591
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-05-03 17:32:14 -0400

    Add bg for hero component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
+import Image from "next/image";
 import React from "react";
-
+import backgroundImg from "../public/bgpattern.svg";
 const HomePage = () => {
   return (
-    <main className='pt-4 text-3xl font-bold flex-grow'>Hello world!</main>
+    <section className='pt-4 text-3xl font-bold flex-grow'>
+      <h1>Hello world!</h1>
+      <Image
+        src={backgroundImg}
+        objectFit='cover'
+        style={{ zIndex: -1 }}
+        layout='fill'
+        quality={100}
+      />
+    </section>
   );
 };
 
