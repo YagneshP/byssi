35d3c7d5bdc0432a99fc7313901b391f076d6812
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-05-10 08:16:03 -0400

    Add height according to screen

--- component/Hero.js
+++ component/Hero.js
@@ -3,14 +3,19 @@ import Image from "next/image";
 import backgroundImg from "../public/bgpattern.svg";
 const Hero = () => {
   return (
-    <section className='pt-4 text-3xl font-bold grow'>
-      <Image
+    <section className='pt-4 text-3xl font-bold grow h-screen bg-[url("/bgpattern.svg")] bg-no-repeat bg-cover'>
+      {/* <Image
         src={backgroundImg}
         objectFit='cover'
-        className='-z-2'
+        className='z-0'
         layout='fill'
         quality={100}
-      />
+      /> */}
+      <div>
+        <h2 className='text-green-600'>
+          Black Youth School Success Initiative
+        </h2>
+      </div>
     </section>
   );
 };
