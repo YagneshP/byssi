29bf6a1d062a7100affdef1a0f8baf063c194d57
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-05-15 19:26:50 -0400

    Add Byssi purpose

--- component/About.js
+++ component/About.js
@@ -11,6 +11,16 @@ function About() {
         </div>
         <div>
           <h3>Description</h3>
+          <p>
+            The Black Youth School Success Initiative (BYSSI) was created to
+            foster and support the educational achievement of Black youth. As an
+            ethno-specific collaborative, BYSSI partners with a community of
+            Black-led/focused organizations to empower youth and fill identified
+            service gaps in the Black community of Peel Region. Building the
+            capacities of collaborating organizations by increasing their
+            assets, attributes and abilities to respond to the complex needs of
+            Black youth and families is at the core of BYSSI.
+          </p>
         </div>
       </div>
     </section>
