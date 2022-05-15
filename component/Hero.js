12293fe229e2cf422b7f53c896ecab373026d0a3
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-05-15 19:14:54 -0400

    Change style for home component

--- component/Hero.js
+++ component/Hero.js
@@ -3,17 +3,16 @@ import Image from "next/image";
 import new_hero from "../public/new_hero.svg";
 const Hero = () => {
   return (
-    <section className='pt-4 text-3xl font-bold grow md:h-screen grid md:grid-cols-2 md:gap-x-8 text-center'>
-      <div>
-        <h2 className='text-green-600 md:text-6xl tracking-widest font-serif  font-bold '>
+    <section className='pt-4 text-3xl font-bold grow h-screen grid sm:grid-rows-2 md:grid-cols-2 md:gap-x-8 text-center place-content-center'>
+      <div className='order-2'>
+        <h2 className='text-stone-500 md:text-6xl tracking-widest font-serif  font-bold '>
           Black Youth <span>School Success</span> Initiative
         </h2>
       </div>
-      <div>
+      <div className='order-1'>
         <Image src={new_hero} alt='A kid is writing on paper' />
       </div>
     </section>
-    // <section className='bg-[url("/new_hero.png")] bg-no-repeat md:bg-cover bg-contain grow h-screen '></section>
   );
 };
 
