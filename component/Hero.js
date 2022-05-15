8c24921274686281581e4cdd8b83aa141de2815e
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-05-15 19:26:23 -0400

    Add container style to section

--- component/Hero.js
+++ component/Hero.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import new_hero from "../public/new_hero.svg";
 const Hero = () => {
   return (
-    <section className='pt-4 text-3xl font-bold grow h-screen grid sm:grid-rows-2 md:grid-cols-2 md:gap-x-8 text-center place-content-center'>
+    <section className='container mx-auto pt-4 text-3xl font-bold grow h-screen grid sm:grid-rows-2 md:grid-cols-2 md:gap-x-8 text-center place-content-center'>
       <div className='order-2'>
         <h2 className='text-stone-500 md:text-6xl tracking-widest font-serif  font-bold '>
           Black Youth <span>School Success</span> Initiative
