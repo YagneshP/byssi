0fe9e67b9399581b20e1666d1b1d1190ff8ebd6a
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-05-15 19:14:22 -0400

    Add about component on index page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Link from "next/link";
 import React from "react";
+import About from "../component/About";
 import Hero from "../component/Hero";
 
 const Home = () => {
   return (
     <main className='grow'>
       <Hero />
+      <About />
     </main>
   );
 };
