6d0560337016eb8362434ae61a24a370ae72528d
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-05-16 09:46:02 -0400

    Add education section

--- component/Services.js
+++ component/Services.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 import React from "react";
 import teacher from "../public/teacher.svg";
 import mentor from "../public/mentor.svg";
+import education from "../public/education.svg";
+
 const Services = () => {
   return (
     <section className='container mx-auto mb-4'>
@@ -16,7 +18,7 @@ const Services = () => {
             </h4>
           </div>
 
-          <p>
+          <p className='p-4'>
             One-to-One Tutoring and Weekly Homework Help as well as Drop-in
             Sessions with your online tutor.
           </p>
@@ -32,24 +34,25 @@ const Services = () => {
             </h4>
           </div>
 
-          <p>
+          <p className='p-4'>
             Big Brother Big Sisters, Leadership and Career Skills Training,
             Parent Ambassador Program.
           </p>
         </div>
-        <div className='md:w-1/3 h-72 bg-amber-200 flex flex-col justify-center pt-4'>
+        <div className='md:w-1/3 h-72 bg-zinc-800 flex flex-col justify-center pt-4'>
           <div className='flex justify-evenly items-center relative'>
             <div className='text-white before:h-24 before:w-24 before:bg-gray-50/40 before:rounded-full before:px-1 before:py-4 before:-top-10 before:left-1 block before:absolute'>
-              <Image src={teacher} width={80} height={80} />
+              <Image src={education} width={80} height={80} />
             </div>
             <h4 className='text-xl font-medium font-serif tracking-wide'>
-              Online Tutoring
+              Online Programing
             </h4>
           </div>
 
-          <p>
-            One-to-One Tutoring and Weekly Homework Help as well as Drop-in
-            Sessions with your online tutor.
+          <p className='p-4'>
+            Virtual programing, educational workshops and events helping
+            students to develop academic, professional and practical life skills
+            and knowledge.
           </p>
         </div>
       </div>
