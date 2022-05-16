4280b4393e23bfd43fc78c95e666ea576c2f6e85
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-05-16 09:34:52 -0400

    Add Icon for Online teaching

---
+++ component/Services.js
@@ -0,0 +1,60 @@
+import Image from "next/image";
+import React from "react";
+import teacher from "../public/teacher.svg";
+
+const Services = () => {
+  return (
+    <section className='container mx-auto mb-4'>
+      <div className='flex md:justify-evenly  flex-col md:flex-row mx-4 gap-4 text-white'>
+        <div className='md:w-1/3 h-72 bg-orange-500 flex flex-col justify-center pt-4'>
+          <div className='flex justify-evenly items-center relative'>
+            <div className='text-white before:h-24 before:w-24 before:bg-gray-50/40 before:rounded-full before:px-1 before:py-4 before:-top-10 before:left-1 block before:absolute'>
+              <Image src={teacher} width={80} height={80} />
+            </div>
+            <h4 className='text-xl font-medium font-serif tracking-wide'>
+              Online Tutoring
+            </h4>
+          </div>
+
+          <p>
+            One-to-One Tutoring and Weekly Homework Help as well as Drop-in
+            Sessions with your online tutor.
+          </p>
+        </div>
+
+        <div className='md:w-1/3 h-72 bg-red-500 flex flex-col justify-center pt-4'>
+          <div className='flex justify-evenly items-center relative'>
+            <div className='text-white before:h-24 before:w-24 before:bg-gray-50/40 before:rounded-full before:px-1 before:py-4 before:-top-10 before:left-1 block before:absolute'>
+              <Image src={teacher} width={80} height={80} />
+            </div>
+            <h4 className='text-xl font-medium font-serif tracking-wide'>
+              Online Tutoring
+            </h4>
+          </div>
+
+          <p>
+            One-to-One Tutoring and Weekly Homework Help as well as Drop-in
+            Sessions with your online tutor.
+          </p>
+        </div>
+        <div className='md:w-1/3 h-72 bg-amber-200 flex flex-col justify-center pt-4'>
+          <div className='flex justify-evenly items-center relative'>
+            <div className='text-white before:h-24 before:w-24 before:bg-gray-50/40 before:rounded-full before:px-1 before:py-4 before:-top-10 before:left-1 block before:absolute'>
+              <Image src={teacher} width={80} height={80} />
+            </div>
+            <h4 className='text-xl font-medium font-serif tracking-wide'>
+              Online Tutoring
+            </h4>
+          </div>
+
+          <p>
+            One-to-One Tutoring and Weekly Homework Help as well as Drop-in
+            Sessions with your online tutor.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import Link from "next/link";
 import React from "react";
 import About from "../component/About";
 import Hero from "../component/Hero";
+import Services from "../component/Services";
 
 const Home = () => {
   return (
     <main className='grow'>
       <Hero />
       <About />
+      <Services />
     </main>
   );
 };
