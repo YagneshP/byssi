34230a362df301b41f1b1944f38615b40e3886f5
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-05-15 23:19:37 -0400

    Add style to about section of the home page

--- component/About.js
+++ component/About.js
@@ -3,15 +3,16 @@ import React from "react";
 import about_us from "../public/about_us.jpg";
 function About() {
   return (
-    <section className='container mx-auto'>
-      <h2>About us</h2>
-      <div className='flex'>
-        <div>
-          <Image src={about_us} width={400} height={300} />
-        </div>
-        <div>
-          <h3>Description</h3>
-          <p>
+    <section className='container mx-auto mb-4'>
+      <h2 className='text-2xl font-semibold text-orange-600 relative mb-4'>
+        <span className='block absolute w-12 rounded-md h-1 bg-orange-400 top-4 '></span>
+        <span className='ml-16'>About us</span>
+      </h2>
+      <div className='flex flex-col md:flex-row justify-between items-center'>
+        <Image src={about_us} width={600} height={300} />
+
+        <div className='md:max-w-md lg:max-w-lg mx-4'>
+          <p className='pt-4 md:pt-0'>
             The Black Youth School Success Initiative (BYSSI) was created to
             foster and support the educational achievement of Black youth. As an
             ethno-specific collaborative, BYSSI partners with a community of
