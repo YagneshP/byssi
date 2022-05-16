5f2d1c67159e33882da07fa3b02ee9ef4b1b817d
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-05-16 09:40:20 -0400

    Add online mentoring section

--- component/Services.js
+++ component/Services.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import teacher from "../public/teacher.svg";
-
+import mentor from "../public/mentor.svg";
 const Services = () => {
   return (
     <section className='container mx-auto mb-4'>
@@ -25,16 +25,16 @@ const Services = () => {
         <div className='md:w-1/3 h-72 bg-red-500 flex flex-col justify-center pt-4'>
           <div className='flex justify-evenly items-center relative'>
             <div className='text-white before:h-24 before:w-24 before:bg-gray-50/40 before:rounded-full before:px-1 before:py-4 before:-top-10 before:left-1 block before:absolute'>
-              <Image src={teacher} width={80} height={80} />
+              <Image src={mentor} width={80} height={80} />
             </div>
             <h4 className='text-xl font-medium font-serif tracking-wide'>
-              Online Tutoring
+              Online Mentoring
             </h4>
           </div>
 
           <p>
-            One-to-One Tutoring and Weekly Homework Help as well as Drop-in
-            Sessions with your online tutor.
+            Big Brother Big Sisters, Leadership and Career Skills Training,
+            Parent Ambassador Program.
           </p>
         </div>
         <div className='md:w-1/3 h-72 bg-amber-200 flex flex-col justify-center pt-4'>
