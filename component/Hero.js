9274224615c80b15441f0de573f321b7d00b619c
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-05-20 19:11:16 -0400

    Replace padding top to items-center

--- component/Hero.js
+++ component/Hero.js
@@ -6,7 +6,7 @@ import Button from "./UI/Button";
 const Hero = () => {
   return (
     <section className='font-bold grow h-screen bg-gradient-to-r from-purple-300/60 to-violet-100'>
-      <div className='container mx-auto h-screen flex flex-col md:pt-16 gap-x-12 justify-evenly sm:flex-row'>
+      <div className='container mx-auto h-screen flex flex-col md:items-center gap-x-12 justify-evenly sm:flex-row'>
         <div className='order-2 md:order-1 mx-4 flex flex-col justify-evenly md:block md:basis-1/2'>
           <h2 className='text-black text-4xl tracking-wide sm:tracking-widest font-serif text-center md:text-left md:leading-normal md:text-6xl font-bold my-2'>
             Black Youth <span className='text-violet-600'>School Success</span>{" "}
