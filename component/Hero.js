a0d4acb20dca4fceef2025bc6ceaf0cb33288139
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-05-20 16:57:13 -0400

    Add style to hero section

--- component/Hero.js
+++ component/Hero.js
@@ -1,17 +1,27 @@
 import React from "react";
 import Image from "next/image";
 import new_hero from "../public/Group.svg";
+import Button from "./UI/Button";
 
 const Hero = () => {
   return (
-    <section className='container mx-auto pt-4 text-3xl font-bold grow h-screen grid sm:grid-rows-2 md:grid-cols-2 md:gap-x-8 text-center place-content-center bg-violet-100/80'>
-      <div className='order-2'>
-        <h2 className='text-stone-500 md:text-6xl tracking-widest font-serif  font-bold '>
-          Black Youth <span>School Success</span> Initiative
-        </h2>
-      </div>
-      <div className='order-1'>
-        <Image src={new_hero} alt='A kid is writing on paper' />
+    <section className='font-bold grow h-screen bg-violet-100/80'>
+      <div className='container mx-auto flex flex-col items-center justify-evenly sm:flex-row'>
+        <div className='order-2 md:order-1 mx-4 flex flex-col justify-evenly md:block'>
+          <h2 className='text-black text-4xl tracking-wide sm:tracking-widest font-serif text-center md:text-left font-bold py-4'>
+            Black Youth <span className='text-violet-600'>School Success</span>{" "}
+            Initiative
+          </h2>
+          <p className='text-sm font-light py-2'>
+            Support up to approximately 500 Black children and youth in grades
+            6-8 and for all four years of high school over a five-year period to
+            achieve graduation rates on par with the regional average.
+          </p>
+          <Button className='my-4'>Enroll student</Button>
+        </div>
+        <div className='order-1 md:order-2 mx-4'>
+          <Image src={new_hero} alt='A kid is writing on paper' />
+        </div>
       </div>
     </section>
   );
