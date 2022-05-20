eb8cbeaff154b90cb531e2c6b59c09049b126810
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-05-20 15:33:35 -0400

    replace new hero image

--- component/Hero.js
+++ component/Hero.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Image from "next/image";
-import new_hero from "../public/new_hero.svg";
+import new_hero from "../public/Group.svg";
+
 const Hero = () => {
   return (
-    <section className='container mx-auto pt-4 text-3xl font-bold grow h-screen grid sm:grid-rows-2 md:grid-cols-2 md:gap-x-8 text-center place-content-center'>
+    <section className='container mx-auto pt-4 text-3xl font-bold grow h-screen grid sm:grid-rows-2 md:grid-cols-2 md:gap-x-8 text-center place-content-center bg-violet-100/80'>
       <div className='order-2'>
         <h2 className='text-stone-500 md:text-6xl tracking-widest font-serif  font-bold '>
           Black Youth <span>School Success</span> Initiative
