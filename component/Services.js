d51b84b4bee33f9b419b806f2d695f7cc0480b82
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-05-24 20:06:32 -0400

    Add box-shadow and mobile style

--- component/Services.js
+++ component/Services.js
@@ -23,18 +23,18 @@ const Services = () => {
     },
   ];
   return (
-    <section className='bg-slate-100 h-screen'>
-      <div className='container mx-auto pt-16'>
+    <section className='bg-slate-100 h-full sm:h-screen'>
+      <div className='container mx-auto pt-16 pb-16'>
         <h2 className='text-2xl font-semibold text-orange-600 relative '>
           <span className='block absolute w-12 rounded-md h-1 bg-orange-400 top-4 '></span>
           <span className='ml-16'>Services</span>
         </h2>
-        <div className='flex md:justify-evenly flex-col md:flex-row mt-32 gap-4 text-slate-600'>
+        <div className='flex md:justify-evenly flex-col justify-between md:flex-row sm:mt-24 gap-4 text-slate-600'>
           {/* New design for services */}
           {services.map((service) => (
             <div
               key={service.title}
-              className='bg-white p-10 rounded-lg relative md:basis-1/3'
+              className='bg-white p-10 rounded-lg mx-4 relative md:basis-1/3 mt-24 shadow-xl'
             >
               <div className='bg-indigo-500 shadow-lg shadow-indigo-500/40 h-20 w-20 text-center p-4 rounded-2xl absolute -top-16 left-10'>
                 <Image src={service.image} />
