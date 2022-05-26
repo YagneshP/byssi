70b09b5bf3f9bcaf543b8d97dd10c2d7d1e718ae
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-05-25 22:41:32 -0400

    Add Teams component

---
+++ component/Teams.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import React from "react";
+
+const Teams = () => {
+  return (
+    <section>
+      <div>
+        <div>
+          <Image />
+        </div>
+        <p>Chenai Kadungure</p>
+        <p>Senior Manager</p>
+      </div>
+    </section>
+  );
+};
+
+export default Teams;
