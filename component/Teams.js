f6f51e1cf99357ce4a0b6bb86f25543cc3100c02
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-05-26 12:35:53 -0400

    Add card style for team member

--- component/Teams.js
+++ component/Teams.js
@@ -6,19 +6,21 @@ const Teams = () => {
   return (
     <section className='conatainer mx-auto'>
       <div className='flex flex-row items-center justify-around gap-10'>
-        <div className='bg-green-200 relative text-center w-72 rounded-2xl '>
-          <figure className='rounded-full border-4 border-yellow-300 ring ring-red-300 -ring-offset-6  relative -top-20 left-16 h-40 w-40'>
+        <div className='bg-slate-300 relative text-center w-72 h-52 rounded-2xl flex flex-col justify-center'>
+          <div className='rounded-full border-4 border-white  ring ring-yellow-300 ring-offset-2 absolute -top-20 left-16 h-40  w-40 '>
             <Image
               src={chenai}
               layout='fill'
               className='absolute rounded-full '
             />
-          </figure>
-          <h3>Chenai Kadungure</h3>
-          <p>Senior Manager</p>
+          </div>
+          <div className='mt-16'>
+            <h3 className='text-2xl font-medium'>Chenai Kadungure</h3>
+            <p className='text-lg'>Senior Manager</p>
+          </div>
         </div>
         <div className='bg-green-200 relative'>
-          <figure className='rounded-full border-4 border-yellow-300 ring ring-red-300 -ring-offset-6  relative h-60 w-60'>
+          <figure className='rounded-full border-4 border-yellow-300 ring ring-red-300 -ring-offset-10  relative h-60 w-60'>
             <Image
               src={chenai}
               layout='fill'
