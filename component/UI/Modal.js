544ee3de2a7a001025edef83e14840f10d2a2fa1
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-06-10 15:00:22 -0400

    Add styling for scrollable modal

--- component/UI/Modal.js
+++ component/UI/Modal.js
@@ -10,22 +10,27 @@ const Modal = () => {
       onClose={() => setIsOpen(false)}
       className='relative z-50'
     >
-      <div className='fixed inset-0 bg-black/30' aria-hidden='true' />
-      <div className='fixed inset-0 flex items-center justify-center p-4'>
-        <Dialog.Panel className='mx-auto max-w-sm rounded bg-white p-4'>
-          <Dialog.Title>Deactivate account</Dialog.Title>
-          <Dialog.Description>
-            This will permanently deactivate your account
-          </Dialog.Description>
+      {/* <div className='fixed inset-0 bg-black/30' aria-hidden='true' /> */}
+      <div
+        className='fixed inset-0 flex items-center justify-center p-4 bg-black/30'
+        aria-hidden='true'
+      >
+        <div className='fixed min-h-full inset-0 flex items-center justify-center p-4'>
+          <Dialog.Panel className='mx-auto max-w-sm rounded bg-white p-4'>
+            <Dialog.Title>Deactivate account</Dialog.Title>
+            <Dialog.Description>
+              This will permanently deactivate your account
+            </Dialog.Description>
 
-          <p>
-            Are you sure you want to deactivate your account? All of your data
-            will be permanently removed. This action cannot be undone.
-          </p>
+            <p>
+              Are you sure you want to deactivate your account? All of your data
+              will be permanently removed. This action cannot be undone.
+            </p>
 
-          <button onClick={() => setIsOpen(false)}>Deactivate</button>
-          <button onClick={() => setIsOpen(false)}>Cancel</button>
-        </Dialog.Panel>
+            <button onClick={() => setIsOpen(false)}>Deactivate</button>
+            <button onClick={() => setIsOpen(false)}>Cancel</button>
+          </Dialog.Panel>
+        </div>
       </div>
     </Dialog>
   );
