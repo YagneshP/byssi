bd8ca805e2ceecbcffa77f82ffc30958e0adeca2
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-06-10 14:30:56 -0400

    Add padding to the modal

--- component/UI/Modal.js
+++ component/UI/Modal.js
@@ -8,7 +8,7 @@ const Modal = () => {
     <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
       <div className='fixed inset-0 bg-black/30' aria-hidden='true' />
       <div className='fixed inset-0 flex items-center justify-center p-4'>
-        <Dialog.Panel>
+        <Dialog.Panel className='mx-auto max-w-sm rounded bg-white p-4'>
           <Dialog.Title>Deactivate account</Dialog.Title>
           <Dialog.Description>
             This will permanently deactivate your account
