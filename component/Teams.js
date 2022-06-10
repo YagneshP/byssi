3e6449cb885faade336069f17343e1baed0e1779
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-06-10 14:23:48 -0400

    Add overlay modal for team member

--- component/Teams.js
+++ component/Teams.js
@@ -5,8 +5,10 @@ import emma from "../public/emma.png";
 import crystel from "../public/crystel.png";
 import parker from "../public/parker.png";
 import sheldon from "../public/sheldon.png";
+import Modal from "./UI/Modal";
 
 const Teams = () => {
+  const [isOpen, setIsOpen] = React.useState(true);
   const teamMembers = [
     {
       employeeName: "Chenai Kadungure",
@@ -37,31 +39,34 @@ const Teams = () => {
     },
   ];
   return (
-    <section className='conatainer mx-auto h-full mb-10'>
-      <h2 className='text-4xl text-center font-extrabold mt-16 '>
-        Meet our Team
-      </h2>
-      <div className='flex flex-row flex-wrap items-center justify-around mt-8 container mx-auto gap-4 '>
-        {teamMembers.map((member) => (
-          <div
-            key={member.employeeName}
-            className='bg-slate-300 relative drop-shadow-lg text-center w-72 h-52 rounded-2xl flex flex-col justify-center mt-32'
-          >
-            <div className='rounded-full border-4 border-white  ring ring-yellow-300 ring-offset-2 absolute -top-20 left-16 h-40  w-40 '>
-              <Image
-                src={member.employeeProfilePic}
-                layout='fill'
-                className='absolute rounded-full '
-              />
+    <>
+      {isOpen && <Modal />}
+      <section className='conatainer mx-auto h-full mb-10'>
+        <h2 className='text-4xl text-center font-extrabold mt-16 '>
+          Meet our Team
+        </h2>
+        <div className='flex flex-row flex-wrap items-center justify-around mt-8 container mx-auto gap-4 '>
+          {teamMembers.map((member) => (
+            <div
+              key={member.employeeName}
+              className='bg-slate-300 relative drop-shadow-lg text-center w-72 h-52 rounded-2xl flex flex-col justify-center mt-32'
+            >
+              <div className='rounded-full border-4 border-white  ring ring-yellow-300 ring-offset-2 absolute -top-20 left-16 h-40  w-40 '>
+                <Image
+                  src={member.employeeProfilePic}
+                  layout='fill'
+                  className='absolute rounded-full '
+                />
+              </div>
+              <div className='mt-16'>
+                <h3 className='text-2xl font-medium'>{member.employeeName}</h3>
+                <p className='text-lg'>{member.employeeTitle}</p>
+              </div>
             </div>
-            <div className='mt-16'>
-              <h3 className='text-2xl font-medium'>{member.employeeName}</h3>
-              <p className='text-lg'>{member.employeeTitle}</p>
-            </div>
-          </div>
-        ))}
-      </div>
-    </section>
+          ))}
+        </div>
+      </section>
+    </>
   );
 };
 
--- component/UI/Modal.js
+++ component/UI/Modal.js
@@ -6,20 +6,23 @@ const Modal = () => {
 
   return (
     <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
-      <Dialog.Panel>
-        <Dialog.Title>Deactivate account</Dialog.Title>
-        <Dialog.Description>
-          This will permanently deactivate your account
-        </Dialog.Description>
+      <div className='fixed inset-0 bg-black/30' aria-hidden='true' />
+      <div className='fixed inset-0 flex items-center justify-center p-4'>
+        <Dialog.Panel>
+          <Dialog.Title>Deactivate account</Dialog.Title>
+          <Dialog.Description>
+            This will permanently deactivate your account
+          </Dialog.Description>
 
-        <p>
-          Are you sure you want to deactivate your account? All of your data
-          will be permanently removed. This action cannot be undone.
-        </p>
+          <p>
+            Are you sure you want to deactivate your account? All of your data
+            will be permanently removed. This action cannot be undone.
+          </p>
 
-        <button onClick={() => setIsOpen(false)}>Deactivate</button>
-        <button onClick={() => setIsOpen(false)}>Cancel</button>
-      </Dialog.Panel>
+          <button onClick={() => setIsOpen(false)}>Deactivate</button>
+          <button onClick={() => setIsOpen(false)}>Cancel</button>
+        </Dialog.Panel>
+      </div>
     </Dialog>
   );
 };
