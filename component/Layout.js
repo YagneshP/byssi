84dc90054dd4a88ce9f7ed51c4109bd1eee7d78a
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-06-10 10:51:31 -0400

    change props name

--- component/Layout.js
+++ component/Layout.js
@@ -11,7 +11,7 @@ const Layout = ({ children }) => {
         <link
           rel='preconnect'
           href='https://fonts.gstatic.com'
-          crossorigin='true'
+          crossOrigin='true'
         />
         <link
           href='https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&family=Yeseva+One&display=swap'
