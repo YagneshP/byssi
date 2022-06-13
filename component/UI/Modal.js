ea0c60353588de673ffaa476a9eb47e32c4d922a
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-06-13 10:59:46 -0400

    Add style to Modal

--- component/UI/Modal.js
+++ component/UI/Modal.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Dialog } from "@headlessui/react";
 
-const Modal = () => {
+const Modal = ({ teammember }) => {
   let [isOpen, setIsOpen] = useState(true);
 
   return (
@@ -10,14 +10,14 @@ const Modal = () => {
       onClose={() => setIsOpen(false)}
       className='relative z-50'
     >
-      {/* <div className='fixed inset-0 bg-black/30' aria-hidden='true' /> */}
       <div
         className='fixed inset-0 flex items-center justify-center p-4 bg-black/30'
         aria-hidden='true'
       >
         <div className='fixed min-h-full inset-0 flex items-center justify-center p-4'>
-          <Dialog.Panel className='mx-auto max-w-sm rounded bg-white p-4'>
-            <Dialog.Title>Deactivate account</Dialog.Title>
+          <Dialog.Panel className='mx-auto max-w-sm rounded bg-white p-4 flex md:flex-row flex-col'>
+            <Dialog.Title>{teammember?.employeeName}</Dialog.Title>
+
             <Dialog.Description>
               This will permanently deactivate your account
             </Dialog.Description>
