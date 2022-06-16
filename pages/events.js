0b98680cfeb6427c1146b40ad3c73b79b47f6ac4
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-06-16 18:53:30 -0400

    Add Events button

---
+++ pages/events.js
@@ -0,0 +1,19 @@
+const events = () => {
+  return (
+    <main className='grow mt-16 sm:mt-24 flex  flex-col items-center'>
+      <div className='px-6 sm:px-4 xl:px-0 w-full max-w-screen-lg mb-16'>
+        <div className='pt-3 pb-1..5 bg-white z-10'>
+          <div className='flex flex-row pb-3'>
+            <div>
+              <button className='outline-none mr-4 text-xl font-medium border-b-4'>
+                Events
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default events;
