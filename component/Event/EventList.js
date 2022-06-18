af7a49606fbd30e2c3b027ec071c645fe7eb7728
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-06-18 09:58:31 -0400

    Change the conditional logic for rendering events

--- component/Event/EventList.js
+++ component/Event/EventList.js
@@ -2,7 +2,7 @@ import React from "react";
 import EventItem from "./EventItem";
 
 const EventList = ({ events }) => {
-  let eventList = events ? (
+  let eventList = !events ? (
     <h3>No event</h3>
   ) : (
     events.map((ev) => <EventItem key={ev._id} event={ev} />)
