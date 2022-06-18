73bbf90f8a7c0e51d4592913492e8cfec9554d80
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-06-18 05:17:58 -0400

    Creating different event header component

---
+++ component/EventHeader.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const EventHeader = () => {
+  return (
+    <div className='flex flex-row  pb-6'>
+      <div>
+        <button className='outline-none mr-4 text-xl font-medium border-b-4 border-rose-60'>
+          Events
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default EventHeader;
