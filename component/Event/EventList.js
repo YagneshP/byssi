539c149e783c6f7df07ead50df38e032af291fe9
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-06-21 16:34:10 -0400

    Add event name as key

--- component/Event/EventList.js
+++ component/Event/EventList.js
@@ -5,10 +5,10 @@ const EventList = ({ events }) => {
   let eventList = !events ? (
     <h3>No event</h3>
   ) : (
-    events.map((ev) => <EventItem key={ev._id} event={ev} />)
+    events.map((ev) => <EventItem key={ev.name} event={ev} />)
   );
 
-  return <div className='flex'>{eventList}</div>;
+  return <div className='flex flex-col'>{eventList}</div>;
 };
 
 export default EventList;
