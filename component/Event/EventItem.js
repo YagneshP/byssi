dad9d1cd8218125a201ce3adc64c9e9672e85be6
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-06-21 15:37:00 -0400

    passing poster as props

--- component/Event/EventItem.js
+++ component/Event/EventItem.js
@@ -4,14 +4,14 @@ import EventTime from "./EventTime";
 import EventTitle from "./EventTitle";
 
 const EventItem = ({ event }) => {
-  const { name, date, host } = event;
+  const { name, date, host, poster } = event;
   return (
     <div className='p-0 bg-clip-padding bg-cover relative h-full flex bg-white z-0 break-words w-full flex-row justify-start py-4 border-t border-gray3 md:pt-4 md:pb-5'>
       <a className='w-full inline cursor-pointer relative hover:no-underline'>
         <div className='flex w-full flex-col'>
           <div className='flex flex-row-reverse md:flex-row flex-1 overflow-hidden'>
             <div className='bg-red-100 ml-3 md:mr-3 md:ml-0'>
-              <EventImage />
+              <EventImage image={poster} />
             </div>
             <div className='overflow-hidden w-full'>
               <EventTime time={date} />
